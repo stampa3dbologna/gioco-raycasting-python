1b7bf00e80f47e3226705658709bbe7a6978bf38
stampa3dbologna <bastia.dario@gmail.com>
2025-08-05 18:28:24 +0200

    funcion.py
    funzioni di base aggunte

--- funcion.py
+++ funcion.py
@@ -1,3 +1,5 @@
+import os
+
 l0 = ['.']*32
 l1 = ['.']*32
 l2 = ['.']*32
@@ -17,7 +19,30 @@ lf = ['.']*32
 
 r = [l0 , l1 ,l2,l3,l4,l5,l6,l7,l8,l9,la,lb,lc,ld,le,lf]
 
-def blank_screen():
+def draw_screen (x,y,texture):
+	r[y][x] = texture
+	
+def move_character (x,y,character):
+	character["x"] = character["x"] + x
+	character["y"] = character["y"] + y
+	
+	
+	
+def make_character (x,y,texture,name):
+	stats_charcter = {
+	"x":x,
+	"y":y,
+	"texture":texture,
+	"name":name
+	}
+	return stats_charcter
+	
+def draw_character (character):
+	draw_screen (character["x"],character["y"],character["texture"])
+	
+
+def update_screen():
+	os.system('cls')
 	for i in r:
 		printing_line = ""
 		for ii in i:
