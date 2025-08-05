3df93fb624db7a620c24b02aca7ab478204670c0
stampa3dbologna <bastia.dario@gmail.com>
2025-08-05 18:30:00 +0200

    main.py
    ora il terminale si aggiorna in modo ottimizzato e ci si può muovere con wasd lascianodo una scia

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 from funcion import *
+import msvcrt
 
-blank_screen()
+
+player =  make_character (4,7,"a","player")
+draw_character (player)
+update_screen()
+
+
+while True :
+
+	if msvcrt.kbhit():         
+		tasto = msvcrt.getch()
+		if tasto == b'a': 
+			move_character (-1,0,player)
+			draw_character (player)
+			update_screen()
+		if tasto == b'd': 
+			move_character (1,0,player)
+			draw_character (player)
+			update_screen()
+		if tasto == b'w': 
+			move_character (0,-1,player)
+			draw_character (player)
+			update_screen()
+		if tasto == b's': 
+			move_character (0,1,player)
+			draw_character (player)
+			update_screen()
