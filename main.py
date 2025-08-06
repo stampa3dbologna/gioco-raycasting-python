b58c5aea1ea9c0534973607bea626475f9a5abd3
stampa3dbologna <bastia.dario@gmail.com>
2025-08-06 19:48:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from bresenham_alg import bresenham_line
 
 player =  make_character (4,7,"a", "31" ,"player")
 draw_character (player)
-update_screen()
-print (bresenham_line(2,2,7,8))
+
+
 draw_line(2,2,7,8,"a",31)
+update_screen()
 
 
 while True :
@@ -29,3 +30,4 @@ while True :
 			move_character (0,1,player)
 			draw_character (player)
 			update_screen()
+
