3ac0ad8bf6f245d236826c81f86f667b69827c33
stampa3dbologna <bastia.dario@gmail.com>
2025-08-07 16:13:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ player =  make_character (4,7,"a", "31" ,"player")
 draw_character (player)
 
 draw_map(map1)
-draw_line(2,2,7,8,"a",31)
 update_screen()
 
 
@@ -16,19 +15,19 @@ while True :
 	if msvcrt.kbhit ():
 		tasto = msvcrt.getch()
 		if tasto == b'a': 
-			move_character (-1,0,player, map1)
+			move_character (-1,0,player)
 			draw_character (player)
 			update_screen()
 		if tasto == b'd': 
-			move_character (1,0,player, map1)
+			move_character (1,0,player)
 			draw_character (player)
 			update_screen()
 		if tasto == b'w': 
-			move_character (0,-1,player, map1)
+			move_character (0,-1,player)
 			draw_character (player)
 			update_screen()
 		if tasto == b's': 
-			move_character (0,1,player, map1)
+			move_character (0,1,player)
 			draw_character (player)
 			update_screen()
 
