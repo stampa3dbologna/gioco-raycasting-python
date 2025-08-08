e8f504482e378bb7da6c0dd289d1197516e77eb1
stampa3dbologna <bastia.dario@gmail.com>
2025-08-08 12:20:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,107 @@ from funcion import *
 from read_file import *
 import msvcrt
 from bresenham_alg import bresenham_line
+import pygame 
+
+
+pygame.init()
+screen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock()
+running = True
+
+
+vel = 2
+x = 300
+y = 300
+player = pygame.Rect(x, y, 10, 10)
+pygame.draw.rect(screen, (0, 255, 0), player)
+
+
+wall = pygame.Rect(500, 500, 50, 10)
+
+a_pressd = None 
+s_pressd = None 
+w_pressd = None 
+d_pressd = None 
+
+
+
+while running:
+
+	screen.fill((0,0,0))
+	
+	#rendo la croce della finestra utilizzabile
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+			
+	#controllo dei tasti premuti
+	if pygame.key.get_just_pressed()[pygame.K_a]: 
+		a_pressd = True
+	
+	if pygame.key.get_just_released()[pygame.K_a]:
+		a_pressd = False
+	
+	if a_pressd :
+		x = x - vel
+		while player.colliderect(wall) :
+			x = x + vel * 2
+			player = pygame.Rect(x,y,10,10)
+	if pygame.key.get_just_pressed()[pygame.K_s]: 
+		s_pressd = True
+	
+	if pygame.key.get_just_released()[pygame.K_s]:
+		s_pressd = False
+	
+	if s_pressd  :
+		y = y + vel
+		while player.colliderect(wall) :
+			y = y - vel * 2
+			player = pygame.Rect(x,y,10,10)
+		
+
+	if pygame.key.get_just_pressed()[pygame.K_w]: 
+		w_pressd = True
+	
+	if pygame.key.get_just_released()[pygame.K_w]:
+		w_pressd = False
+	
+	if w_pressd :
+		y = y - vel
+		while player.colliderect(wall) :
+			y = y + vel * 2
+			player = pygame.Rect(x,y,10,10)
+
+	if pygame.key.get_just_pressed()[pygame.K_d]: 
+		d_pressd = True
+	
+	if pygame.key.get_just_released()[pygame.K_d]:
+		d_pressd = False
+	
+	if d_pressd  :
+		x = x + vel
+		while player.colliderect(wall) :
+			x = x - vel * 2
+			player = pygame.Rect(x,y,10,10)
+	
+	#fine controllo tasti premuti
+	
+	player = pygame.Rect(x,y,10,10)
+	pygame.draw.rect(screen, (0, 255, 0), player)	
+	pygame.draw.rect(screen, (0, 0, 255), wall)
+	
+	pygame.display.flip()
+
+	clock.tick(60)  
+
+pygame.quit()
+
+'''
 
 player =  make_character (4,7,"a", "31" ,"player")
 draw_character (player)
 
 draw_map(map1)
-update_screen()
 
 
 while True :
@@ -31,4 +126,5 @@ while True :
 			draw_character (player)
 			update_screen()
 
+'''
 
