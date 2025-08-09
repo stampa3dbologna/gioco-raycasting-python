30de03a0b53d49b1d1bbca0ba3d2225bfbe75c8e
stampa3dbologna <bastia.dario@gmail.com>
2025-08-09 20:03:21 +0200

    Add files via upload

---
+++ create_vector.py
@@ -0,0 +1,10 @@
+import math
+
+def point_in_direction(x0, y0, distance, angle_degrees):
+
+    angle_radians = math.radians(angle_degrees)
+    x = x0 + distance * math.cos(angle_radians)
+    y = y0 + distance * math.sin(angle_radians)
+
+    return (round(x), round(y))
+	
