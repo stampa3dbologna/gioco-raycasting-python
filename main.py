1feb50bd79de64c98251960a71d3c2a33c4e8e98
ciccio440 <gangemivinicio@gmail.com>
2025-08-10 15:42:52 +0200

    Update main.py
    ho aggiunto i rettangoli per le collisioni

--- main.py
+++ main.py
@@ -40,21 +40,64 @@ while running:
 	m_x , m_y = pygame.mouse.get_rel()
 	
 	#quadrato per mappa
-	squere1 = pygame.Rect(130,220 ,240,90)
+	squere1 = pygame.Rect(376,327 ,555,104)
 	pygame.draw.rect(screen, (0, 0, 255), squere1)
-	squere2 = pygame.Rect(284,492 ,136, 152)
-	pygame.draw.rect(screen, (0, 0, 255), squere2)	
-#	squere3 = pygame.Rect(420,584 ,179, 222)
-#	pygame.draw.rect(screen, (0, 0, 255), squere3)	
-	squere4 = pygame.Rect(564,490 ,830, 158)
-	pygame.draw.rect(screen, (0, 0, 255), squere4) 
-	squere5 = pygame.Rect(566,290 ,194, 40)
-	pygame.draw.rect(screen, (0, 0, 255), squere5) 
-	squere6 = pygame.Rect(768,112 ,180, 56)
-	pygame.draw.rect(screen, (0, 0, 255), squere6) 
+	squere2 = pygame.Rect(191,329 ,184,104)
+	pygame.draw.rect(screen, (0, 0, 255), squere2)
+	squere3 = pygame.Rect(378,194 ,26,132)
+	pygame.draw.rect(screen, (0, 0, 255), squere3)
+	squere4 = pygame.Rect(404,193 ,108,34)
+	pygame.draw.rect(screen, (0, 0, 255), squere4)
+	squere5 = pygame.Rect(476,227 ,36,40)
+	pygame.draw.rect(screen, (0, 0, 255), squere5)
+	squere6 = pygame.Rect(512,237 ,120,30)
+	pygame.draw.rect(screen, (0, 0, 255), squere6)
+	squere7 = pygame.Rect(602,76 ,32,190)
+	pygame.draw.rect(screen, (0, 0, 255), squere7)
+	squere8 = pygame.Rect(10,-1 ,1280,28)
+	pygame.draw.rect(screen, (0, 0, 255), squere8)
+	squere9 = pygame.Rect(126,149 ,120,58)
+	pygame.draw.rect(screen, (0, 0, 255), squere9)
+	squere10 = pygame.Rect(2,23 ,124,184)
+	pygame.draw.rect(screen, (0, 0, 255), squere10)
+	squere11 = pygame.Rect(18,206 ,50,498)
+	pygame.draw.rect(screen, (0, 0, 255), squere11)
+	squere12 = pygame.Rect(54,678 ,390,46)
+	pygame.draw.rect(screen, (0, 0, 255), squere12)
+	squere13 = pygame.Rect(312,552 ,148,168)
+	pygame.draw.rect(screen, (0, 0, 255), squere13)
+	squere14 = pygame.Rect(460,552 ,0,0)
+	pygame.draw.rect(screen, (0, 0, 255), squere14)
+	squere15 = pygame.Rect(458,700 ,542,20)
+	pygame.draw.rect(screen, (0, 0, 255), squere15)
+	squere16 = pygame.Rect(512,76 ,98,34)
+	pygame.draw.rect(screen, (0, 0, 255), squere16)
+	squere17 = pygame.Rect(460,552 ,110,43)
+	pygame.draw.rect(screen, (0, 0, 255), squere17)
+	squere18 = pygame.Rect(644,552 ,446,40)
+	pygame.draw.rect(screen, (0, 0, 255), squere18)
+	squere19 = pygame.Rect(756,592 ,246,108)
+	pygame.draw.rect(screen, (0, 0, 255), squere19)
+	squere20 = pygame.Rect(1020,326 ,70,226)
+	pygame.draw.rect(screen, (0, 0, 255), squere20)
+	squere21 = pygame.Rect(994,590 ,96,31)
+	pygame.draw.rect(screen, (0, 0, 255), squere21)
+	squere22 = pygame.Rect(1000,704 ,284,12)
+	pygame.draw.rect(screen, (0, 0, 255), squere22)
+	squere23 = pygame.Rect(702,84 ,80,56)
+	pygame.draw.rect(screen, (0, 0, 255), squere23)
+	squere24 = pygame.Rect(702,140 ,36,128)
+	pygame.draw.rect(screen, (0, 0, 255), squere24)
+	squere25 = pygame.Rect(738,224 ,354,42)
+	pygame.draw.rect(screen, (0, 0, 255), squere25)
+	squere26 = pygame.Rect(1050,106 ,42,120)
+	pygame.draw.rect(screen, (0, 0, 255), squere26)
+	squere27 = pygame.Rect(1264,96 ,14,614)
+	pygame.draw.rect(screen, (0, 0, 255), squere27)
+	
 
 
-	#fine quadrati mappa
+#fine quadrati mappa
 	
 	
 	if m_x != 0:
@@ -69,7 +112,7 @@ while running:
 	if pygame.key.get_just_released()[pygame.K_a]:
 		a_pressd = False
 	
-	if a_pressd :
+	if a_pressd : 
 		new_x,new_y = point_in_direction(x,y,vel,direction-90)
 		new_player = pygame.Rect(new_x, new_y, 10, 10)
 		if not new_player.colliderect(wall):
@@ -164,5 +207,3 @@ while True :
 			update_screen()
 
 '''
-
-
